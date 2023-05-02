84804223df39153e0030fa49a5adeba98531535d
Your Name <alan.fer09@gmail.com>
2023-05-02 20:59:10 -0300

    add folder 19

---
+++ clase 19- loops-arreglos/arreglos.js
@@ -0,0 +1,54 @@
+var nombre = "alan"
+var nombre2 = "pepe"
+var nombre3 = "juan"
+
+//arreglo
+[]
+//guardamos dentro de una variable
+// esta vacio
+var nombres = [];
+
+//con elementos
+var nombres = ["alan","pepe","juan"];
+
+//podemos guardar todo tipo de datos
+var arr = [12,"hola",true]
+
+//indices
+
+nombres[0] // "alan"
+
+
+// arreglos anidados
+var arr = [1,2,3,4,[5,6,7,8]]
+arr[4][1]//6
+
+//metodos
+
+var arr = []
+//push
+arr.push(1)
+arr.push("hola")
+arr.push(false)
+//[1,"hola",false]
+
+//pop
+
+arr.pop()
+
+//unshift
+arr.unshift(0)
+
+//shift
+arr.shift()
+
+//propiedades
+//length
+//la cantidad de elementos de ese arreglo
+arr.length //3
+
+
+var arr = [1,2,3,4,5]
+for(var i =0;i<arr.length;i++){
+    console.log(arr[i])
+}
---
+++ clase 19- loops-arreglos/loops.js
@@ -0,0 +1,80 @@
+//console.log(1)
+//console.log(2)
+//console.log(3)
+//console.log(4)
+//console.log(5)
+
+//while
+//decrementar
+/*
+var index = 10;
+while(index > 0){
+    console.log(index)
+    index--
+}
+//incrementar
+var index = 1;
+while(index <= 10){
+    console.log(index)
+    index++
+}
+*/
+
+//condicionales
+var index = 1
+while(index <=5){
+    if(index===3){
+       // console.log("tres")
+       
+    }else{
+        //console.log(index)
+    }
+    index++
+}
+
+//imprimir por consola todos los numeros del 1 al 100
+// y si el numero es multiplo de 2, en vez de imprimir el numero, van a imprimir la palabra fizz
+// y si el numero es multiplo de 3, van a imprimir "buzz"
+// y si el numero es multiplo de 3 y multiplo de 5 van a imprimir la palabra fizzbuzz
+/*
+*/
+// var index = 1
+// while(index <=100){
+//     if(index%3==0 && index%5===0){
+//         console.log("fizzbuzz")
+//     }else if(index%3===0){
+//         console.log("buzz")
+//     }
+//     else if(index%2==0){
+//         console.log("fizz")
+//     }
+//     else{
+//         console.log(index)
+//     }
+//     index++
+// }
+
+// var numeroSecreto = 10
+
+// var res = Number(prompt("adivina el numero"))
+// while(res!==numeroSecreto){
+//     alert("no adivinaste el numero, vuelve a intentar")
+//     res= Number(prompt("adivina el numero"))
+// }
+
+// alert("adivinaste el numero")
+
+// for(var index=1;index<=10;index++){
+//     console.log(index)
+// }
+// if(res===numeroSecreto){
+//     alert("adivinaste el numero")
+// }
+// else{
+//     alert("no adivinaste el numero")
+// }
+var i = 8
+do{
+console.log(i)
+i++
+}while(i<=5)
